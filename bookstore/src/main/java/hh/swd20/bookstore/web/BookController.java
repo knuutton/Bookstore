7ef227e7693e485a8b5e39516ddab4c3b34cadd2
knuutton <toni.knuuttila@outlook.com>
2019-02-05 19:42:15 +0200

    first week

---
+++ bookstore/src/main/java/hh/swd20/bookstore/web/BookController.java
@@ -0,0 +1,17 @@
+package hh.swd20.bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.swd20.bookstore.domain.Book;
+
+@Controller
+public class BookController {
+
+	@GetMapping("/index")
+	public String home(Model model) {
+		model.addAttribute("book", new Book());
+		return "index";
+	}
+}
